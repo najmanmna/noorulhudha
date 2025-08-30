46aa9f7e942518a3ba0f7852bef623663e26ee9e
najmanmna <139351540+najmanmna@users.noreply.github.com>
2025-08-30 19:17:28 +0530

    update

--- src/components/WhyChooseUs.jsx
+++ src/components/WhyChooseUs.jsx
@@ -48,7 +48,7 @@ export default function WhyChooseUs() {
         </div>
 
         {/* Features Grid */}
-        <div className="max-w-4xl mx-8 md:mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+        <div className="max-w-4xl mx-14 md:mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
           {features.map((feature, index) => (
             <div
               key={index}
